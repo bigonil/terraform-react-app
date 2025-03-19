afedfb13fb72aa547698432724fc6166c2ea6973
bigonil <luca.bigoni@gmail.com>
2025-03-19 15:08:24 +0100

    Update aws-exports.js

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -3,18 +3,18 @@ import Amplify from "aws-amplify";
 const awsConfig = {
   Auth: {
     region: "us-east-1",
-    userPoolId: "us-east-1_XXXXXXXXX",
-    userPoolWebClientId: "XXXXXXXXXXXXXXXXXXXX",
+    userPoolId: "us-east-1_itRijY08z",
+    userPoolWebClientId: "1cljes4s7rha2jgrinupv69irb",
   },
   Storage: {
-    bucket: "my-react-app-bucket",
+    bucket: "my-react-app-bucket-631737274131",
     region: "us-east-1",
   },
   API: {
     endpoints: [
       {
         name: "GraphQLAPI",
-        endpoint: "https://XXXXXXXXXXXX.appsync-api.us-east-1.amazonaws.com/graphql",
+        endpoint: "https://pvb4mws2vvfdhfdygu2ss3pz3i.appsync-api.us-east-1.amazonaws.com/graphql",
       },
     ],
   },
